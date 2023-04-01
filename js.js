e220128b73bae8db89510265de4427fdae151042
Cheesephu <rcpowerbest@gmail.com>
2023-04-02 02:45:48 +0700

    update

--- js.js
+++ js.js
@@ -69,3 +69,19 @@ $(document).ready(function(){
         }
     });
 });
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
